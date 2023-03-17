8978c6a924bc80c6bd07990e055cffebb5475ab6
med-el-hamidi <dev.elhamidi@gmail.com>
2023-03-17 16:10:18 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (EXIT_SUCCESS)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
