970c5dbfc8a2328d06919971266b4c8d81643aee
med-el-hamidi <dev.elhamidi@gmail.com>
2023-03-16 18:40:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point function
+ * return: Always 0 (EXIT_SUCCESS)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
