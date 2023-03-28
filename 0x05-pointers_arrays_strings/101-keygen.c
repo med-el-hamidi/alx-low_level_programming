168771656c2b508c4ce8ec0a1acb210b59c58474
med-el-hamidi <dev.elhamidi@gmail.com>
2023-03-27 18:30:01 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,45 +2,69 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 9
+#define PASSWORD_LENGTH 100
+#define MAX_DIGIT_VALUE 78
+#define PASSWORD_SUM_TARGET 2772
 
 /**
- * main - generates a random password for 101-crackme
- *
- * Return: always 0
+ * Generates a random password and prints it to stdout.
+ * @param password An array to store the generated password.
+ * @param length The length of the password array.
+ */
+void generate_password(int password[], int length)
+{
+    srand(time(NULL));
+    for (int i = 0; i < length; i++)
+    {
+        password[i] = rand() % MAX_DIGIT_VALUE;
+        putchar(password[i] + '0');
+    }
+}
+
+/**
+ * Calculates the sum of the digits in the password.
+ * @param password An array containing the password digits.
+ * @param length The length of the password array.
+ * @return The sum of the password digits.
+ */
+int calculate_password_sum(int password[], int length)
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += password[i];
+    }
+    return sum;
+}
+
+/**
+ * Adds a digit to the password to make its sum equal to PASSWORD_SUM_TARGET.
+ * @param password An array containing the password digits.
+ * @param length The length of the password array.
+ * @param sum The current sum of the password digits.
+ * @return The digit added to the password.
+ */
+int add_missing_digit(int password[], int length, int sum)
+{
+    int digit_to_add = PASSWORD_SUM_TARGET - sum;
+    password[length - 1] = digit_to_add;
+    putchar(digit_to_add + '0');
+    return digit_to_add;
+}
+
+/**
+ * Main function that generates a random valid password for 101-crackme.
+ * @return Always 0 (Success).
  */
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-    int i;
-
-    /* Initialize the random number generator */
-    srand(time(NULL));
-
-    /* Generate random characters for the password */
-    for (i = 0; i < PASSWORD_LENGTH; i++)
+    int password[PASSWORD_LENGTH];
+    generate_password(password, PASSWORD_LENGTH);
+    int sum = calculate_password_sum(password, PASSWORD_LENGTH);
+    if (sum < PASSWORD_SUM_TARGET)
     {
-        int r = rand() % 62;
-
-        if (r < 26)
-        {
-            password[i] = 'a' + r;
-        }
-        else if (r < 52)
-        {
-            password[i] = 'A' + r - 26;
-        }
-        else
-        {
-            password[i] = '0' + r - 52;
-        }
+        add_missing_digit(password, PASSWORD_LENGTH, sum);
     }
-
-    /* Add null terminator to the end of the password */
-    password[PASSWORD_LENGTH] = '\0';
-
-    /* Print the password */
-    printf("%s\n", password);
-
-    return (0);
+    return 0;
 }
+
