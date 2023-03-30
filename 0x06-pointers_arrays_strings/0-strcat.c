23e9c29e8305054d636a3d6ee0596f7d1d26f4ec
med-el-hamidi <dev.elhamidi@gmail.com>
2023-03-30 15:07:18 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings like strcat.
+ * @dest:input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p)
+	{
+		p++;
+	}
+
+	while (*src)
+	{
+		*p++ = *src++;
+	}
+
+	*p = '\0';
+
+	return (dest);
+}
