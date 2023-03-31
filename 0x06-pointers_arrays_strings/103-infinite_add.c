6eb1c48df79f87f260a05c0fb74b47efcda2f924
med-el-hamidi <dev.elhamidi@gmail.com>
2023-03-30 17:24:09 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,66 +1,77 @@
 #include "main.h"
 
 /**
- * reverse_string - reverse a string
- * @str: the string to reverse
+ * rev_string - reverse array
+ * @n: integer parametrs
  */
-void reverse_string(char *str)
+
+void rev_string(char *n)
 {
-	if (!str)
-		return;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-	int len = 0;
-
-	while (*(str + len) != '\0')
-		len++;
-
-	for (int i = 0; i < len / 2; i++)
+	while (*(n + i) != '\0')
 	{
-		char tmp = *(str + i);
-		*(str + i) = *(str + len - i - 1);
-		*(str + len - i - 1) = tmp;
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
 	}
 }
 
 /**
- * infinite_add - adds two numbers stored as strings
- * @n1: the first number as a string
- * @n2: the second number as a string
- * @r: a buffer to store the result
- * @size_r: the size of the buffer
- *
- * Return: a pointer to the result or NULL if the result cannot be stored
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	if (!n1 || !n2 || !r || size_r < 2)
-		return (NULL);
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	int len1 = 0, len2 = 0, lenr = 0, carry = 0;
-
-	while (*(n1 + len1) != '\0')
-		len1++;
-
-	while (*(n2 + len2) != '\0')
-		len2++;
-
-
-	while (len1 > 0 || len2 > 0 || carry)
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
-		int digit1 = len1 > 0 ? *(n1 + --len1) - '0' : 0;
-		int digit2 = len2 > 0 ? *(n2 + --len2) - '0' : 0;
-		int sum = digit1 + digit2 + carry;
-
-		carry = sum / 10;
-		sum %= 10;
-
-		if (lenr >= size_r - 1)
-			return (NULL);
-
-		*(r + lenr++) = sum + '0';
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
 	}
-	*(r + lenr) = '\0';
-	reverse_string(r);
-
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }
