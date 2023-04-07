06719495b78a6d7ba1750382799a237d603f818e
med-el-hamidi <dev.elhamidi@gmail.com>
2023-04-06 19:36:47 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,49 @@
 #include "main.h"
 
-int is_palindrome_helper(char *s, int start, int end);
+int _strlen_recursion(char *s);
+int check_palindrome(char *s, int start, int end);
 
 /**
- * is_palindrome_helper - Helper function to check if a string is a palindrome.
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
  *
- * @s: The string to check.
- * @start: The starting index of the string.
- * @end: The ending index of the string.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
-int is_palindrome_helper(char *s, int start, int end)
-{
-	if (start >= end)
-	{
-		return (1);
-	}
-	if (s[start] != s[end])
-	{
-		return (0);
-	}
-	return (is_palindrome_helper(s, start + 1, end - 1));
-}
-
-
-/**
- * is_palindrome - Checks if a string is a palindrome.
- *
- * @s: The string to check.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
+ * Return: 1 if it is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
 
 	if (len <= 1)
-	{
 		return (1);
-	}
-	return (is_palindrome_helper(s, 0, len - 1));
+	return (check_palindrome(s, 0, len - 1));
+}
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ * @start: starting index of the string
+ * @end: ending index of the string
+ *
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+int check_palindrome(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (check_palindrome(s, start + 1, end - 1));
 }
