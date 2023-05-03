7c066263e0dcd8691c65e28fa516744de587302a
med-el-hamidi <dev.elhamidi@gmail.com>
2023-05-03 14:28:25 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (EXIT_SUCCESS).
+ */
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
